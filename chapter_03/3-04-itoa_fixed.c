951766b2985a713f4aa0ad2f42599d648cdb72a8
lilibyte <lily@lilibyte.net>
2022-01-28 13:17:40 -0600

    add exercise 3-4

---
+++ chapter_03/3-04-itoa_fixed.c
@@ -0,0 +1,55 @@
+/*
+ * Exercise 3-4.
+ *
+ * In a two's complement number representation, our version of itoa
+ * does not handle the largest negative number, that is, the value
+ * of n equal to -(2^(wordsize-1)). Explain why not. Modify it to
+ * print that value correctly, regardless of the machine on which
+ * it runs.
+*/
+
+#include <limits.h>
+#include <stdio.h>
+#include <string.h>
+
+void reverse(char s[]) {
+  for (int i = 0, j = strlen(s)-1; i < j; i++, j--) {
+    int c = s[i];
+    s[i] = s[j];
+    s[j] = c;
+  }
+}
+
+void itoa(int n, char s[]) {
+  int sign;
+  int test = 0;
+  if ((sign = n) < 0) {
+    // The unary minus invokes undefined behavior due to signed integer
+    // overflow when applied to INT_MIN on 2's complement platforms.
+    // https://en.cppreference.com/w/c/language/operator_arithmetic
+    if (sign == INT_MIN) {
+      test = 1;
+      n += 1;
+    }
+    n = -n;
+  }
+  int i = 0;
+  do {
+    s[i++] = n % 10 + '0';
+  } while ((n /= 10) > 0);
+  if (sign < 0)
+    s[i++] = '-';
+  s[i] = '\0';
+  // probably not capturing the spirit of the exercise but whatever
+  if (test)
+    ++s[0];
+  reverse(s);
+}
+
+int main() {
+  char s1[1000];
+  itoa(INT_MIN+1, s1);
+  printf("%s\n", s1);
+  itoa(INT_MIN, s1);
+  printf("%s\n", s1);
+}
